814fbce4ea53c7f0f16b7add1056733bc96e1d06
Luciano Zimmermann <lucianolz123@gmail.com>
2024-04-11 15:12:05 -0300

    User entity and resource

---
+++ src/main/java/com/luciano/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.luciano.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luciano.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Ane", "ane@gmail.com", "091283", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
